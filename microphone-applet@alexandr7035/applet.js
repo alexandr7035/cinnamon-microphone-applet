93c428ca4b5219dabd1eddaf89080b5f82e5ec47
Alexandr Alexeenko <alexandr7035.adm@gmail.com>
2021-10-08 01:07:34 +0300

    Fix show icon

--- microphone-applet@alexandr7035/applet.js
+++ microphone-applet@alexandr7035/applet.js
@@ -2,13 +2,20 @@ const Applet = imports.ui.applet;
 const Main = imports.ui.main;
 
 class MicApplet extends Applet.IconApplet {
-    constructor(metadata, orientation, panel_height, instance_id) {
-        super(metadata, orientation, panel_height, instance_id)
+    constructor(metadata) {
+        super(metadata)
 
+        global.logError("start applet")
+
+        try {
         // this.set_applet_icon_name("icon");
         this.set_applet_icon_name("mic_icon");
         this.set_applet_tooltip(_("Control microphone level with the applet"));
-
+        }
+        catch (e) {
+            global.logError(e);
+          }
+    
     }
 
     on_applet_clicked() {
@@ -16,6 +23,6 @@ class MicApplet extends Applet.IconApplet {
     }
 }
 
-function main(metadata, orientation, panel_height, instance_id) {
-    return new MicApplet(metadata, orientation, panel_height, instance_id);
+function main(orientation) {
+    return new MicApplet(orientation);
 }
